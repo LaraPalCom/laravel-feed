024cfe3232fdda9ba7afb2b0d6aaa3bd7d0fe000
Roumen Damianoff <roumen@dawebs.com>
2013-07-21 16:53:07 +0300

    updated views

--- src/views/atom.blade.php
+++ src/views/atom.blade.php
@@ -1,28 +1,29 @@
 {{ '<?xml version="1.0" encoding="UTF-8" ?>'."\n" }}
-<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
-    <channel>
-        <title><![CDATA[{{ $channel['title'] }}]]></title>
-        <link>{{ $channel['link'] }}</link>
-        <description><![CDATA[{{ $channel['description'] }}]]></description>
-        <atom:link href="{{ $channel['link'] }}" rel="self"></atom:link>
-        @if (!empty($channel['logo']))
-        <image>
-            <url>{{ $channel['logo'] }}</url>
-            <title><![CDATA[{{ $channel['title'] }}]]></title>
-            <link>{{ $channel['link'] }}</link>
-        </image>
-        @endif
-        <language>{{ $channel['lang'] }}</language>
-        <lastBuildDate>{{ date('D, d M Y H:i:s O', strtotime($channel['pubdate'])) }}</lastBuildDate>
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title type="html">{{ $channel['title'] }}</title>
+    <subtitle type="html">{{ $channel['description'] }}</subtitle>
+    <link href="{{ $channel['link'] }}"></link>
+    <id>{{ $channel['link'] }}</id>
+    <link rel="alternate" type="text/html" href="{{ $channel['link'] }}" ></link>
+    <link rel="self" type="application/atom+xml" href="{{ Request::url() }}" ></link>
+    @if (!empty($channel['logo']))
+    <logo>{{ $channel['logo'] }}</logo>
+    @endif
+    @if (!empty($channel['icon']))
+    <icon>{{ $channel['icon'] }}</icon>
+    @endif
+        <updated>{{ date('c', strtotime($channel['pubdate'])) }}</updated>
         @foreach($items as $item)
-        <item>
-            <title><![CDATA[{{ $item['title'] }}]]></title>
-            <link>{{ $item['link'] }}</link>
-            <guid isPermaLink="true">{{ $item['link'] }}</guid>
-            <description><![CDATA[{{ $item['description'] }}]]></description>
-            <dc:creator xmlns:dc="http://purl.org/dc/elements/1.1/">{{ $item['author'] }}</dc:creator>
-            <pubDate>{{ date('D, d M Y H:i:s O', strtotime($item['pubdate'])) }}</pubDate>
-        </item>
+        <entry>
+            <author>
+                <name>{{ $item['author'] }}</name>
+            </author>
+            <title type="html"><![CDATA[{{ $item['title'] }}]]></title>
+            <link rel="alternate" type="text/html" href="{{ $item['link'] }}"></link>
+            <id>{{ $item['link'] }}</id>
+            <summary type="html"><![CDATA[{{ $item['description'] }}]]>}}</summary>
+            <updated>{{ date('c', strtotime($item['pubdate'])) }}</updated>
+        </entry>
         @endforeach
-    </channel>
-</rss>
+
+</feed>
