d023e7127249826cec88bf63f06c5b971a4541f2
Abhimanyu Sharma <abhimanyusharma003@gmail.com>
2014-06-02 19:50:00 +0530

    Protection of compiling php tags if short open tags are enabled in php settings

--- src/views/atom.blade.php
+++ src/views/atom.blade.php
@@ -1,4 +1,4 @@
-{{ '<?xml version="1.0" encoding="UTF-8" ?>'."\n" }}
+{{ '<'.'?'.'xml version="1.0" encoding="UTF-8" ?>'."\n" }}
 <feed xmlns="http://www.w3.org/2005/Atom">
     <title type="html">{{ $channel['title'] }}</title>
     <subtitle type="html">{{ $channel['description'] }}</subtitle>
