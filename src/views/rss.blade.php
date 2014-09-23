143baf10913a6d5f336fa1c8c69a7f8be85a4673
Roumen Damianoff <roumen@dawebs.com>
2014-09-23 17:45:29 +0300

    added custom namespaces

--- src/views/rss.blade.php
+++ src/views/rss.blade.php
@@ -1,5 +1,5 @@
 {{ '<'.'?'.'xml version="1.0" encoding="UTF-8" ?>'."\n" }}
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/"<?php foreach($namespaces as $n) echo " "+$n; ?>>
     <channel>
         <title><![CDATA[{{ $channel['title'] }}]]></title>
         <link>{{ $channel['link'] }}</link>
