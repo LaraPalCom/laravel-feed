9529cc68942de438cb15d3b0bcfd5154ba8408d7
Roumen Damianoff <roumen@dawebs.com>
2014-09-30 19:38:57 +0300

    tag version 2.7.3

--- src/Roumen/Feed/Feed.php
+++ src/Roumen/Feed/Feed.php
@@ -3,7 +3,7 @@
  * Feed generator class for laravel-feed package.
  *
  * @author Roumen Damianoff <roumen@dawebs.com>
- * @version 2.7.2
+ * @version 2.7.3
  * @link http://roumen.it/projects/laravel-feed
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
