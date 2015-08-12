d36d876f62e4bf57c5bb9ffe540667972264d921
Ivan Klimchuk <klimchuk@1pt.com>
2015-08-12 14:29:27 +0300

    Fixed links in templates for output

--- src/views/atom.blade.php
+++ src/views/atom.blade.php
@@ -1,11 +1,11 @@
-{!! '<'.'?'.'xml version="1.0" encoding="UTF-8" ?>'."\n" !!}
+{!! '<'.'?'.'xml version="1.0" encoding="UTF-8" ?>' !!}
 <feed xmlns="http://www.w3.org/2005/Atom"<?php foreach($namespaces as $n) echo " ".$n; ?>>
     <title type="html">{!! $channel['title'] !!}</title>
     <subtitle type="html">{!! $channel['description'] !!}</subtitle>
-    <link href="{{ $channel['link'] }}"></link>
+    <link href="{{ Request::url() }}"></link>
     <id>{{ $channel['link'] }}</id>
-    <link rel="alternate" type="text/html" href="{{ $channel['link'] }}" ></link>
-    <link rel="self" type="application/atom+xml" href="{{ Request::url() }}" ></link>
+    <link rel="alternate" type="text/html" href="{{ Request::url() }}" ></link>
+    <link rel="self" type="application/atom+xml" href="{{ $channel['link'] }}" ></link>
     @if (!empty($channel['logo']))
     <logo>{{ $channel['logo'] }}</logo>
     @endif
