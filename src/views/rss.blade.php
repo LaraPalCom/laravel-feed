0869e9b9eea4c5b167caeec375500ab998437c8d
Roumen Damianoff <roumen@dawebs.com>
2015-11-02 20:08:51 +0200

    wip #13

--- src/views/rss.blade.php
+++ src/views/rss.blade.php
@@ -1,5 +1,5 @@
 {!! '<'.'?'.'xml version="1.0" encoding="UTF-8" ?>' !!}
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/"<?php foreach($namespaces as $n) echo " ".$n; ?>>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:media="http://search.yahoo.com/mrss/"<?php foreach($namespaces as $n) echo " ".$n; ?>>
     <channel>
         <title><![CDATA[{{ $channel['title'] }}]]></title>
         <link>{{ Request::url() }}</link>
@@ -25,6 +25,35 @@
             @endif
             <dc:creator xmlns:dc="http://purl.org/dc/elements/1.1/">{{ $item['author'] }}</dc:creator>
             <pubDate>{{ $item['pubdate'] }}</pubDate>
+            @if (!empty($item['media:content']))
+            <media:content
+            @foreach ($item['media:content'] as $k => $v)
+            {!! $k.'="'.$v.'" ' !!}
+            @endforeach
+            />
+            @endif
+            @if (!empty($item['media:thumbnail']))
+            <media:thumbnail
+            @foreach ($item['media:thumbnail'] as $k => $v)
+            {!! $k.'="'.$v.'" ' !!}
+            @endforeach
+            />
+            @endif
+            @if (!empty($item['media:title']))
+            <media:title type="plain">{{ $item['media:title'] }}</media:title>
+            @endif
+            @if (!empty($item['media:description']))
+            <media:description type="plain">{{ $item['media:description'] }}</media:description>
+            @endif
+            @if (!empty($item['media:keywords']))
+            <media:keywords>{{ $item['media:title'] }}</media:keywords>
+            @endif
+            @if (!empty($item['media:rating']))
+            <media:rating>{{ $item['media:rating'] }}</media:rating>
+            @endif
+            @if (!empty($item['creativeCommons:license']))
+            <creativeCommons:license>{{ $item['creativeCommons:license'] }}</creativeCommons:license>
+            @endif
         </item>
         @endforeach
     </channel>
