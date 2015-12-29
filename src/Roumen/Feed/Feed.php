d57768c3b51e00056ca10ec5dc6824e2f9c968e5
Roumen Damianoff <roumen@dawebs.com>
2015-12-29 14:07:20 +0200

    update Feed class version

--- src/Roumen/Feed/Feed.php
+++ src/Roumen/Feed/Feed.php
@@ -3,7 +3,7 @@
  * Feed generator class for laravel-feed package.
  *
  * @author Roumen Damianoff <roumen@dawebs.com>
- * @version 2.8.6
+ * @version 2.8.7
  * @link https://roumen.it/projects/laravel-feed
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
