4cd301e5c3db3ad8d0b6970c79a7c94e08cfeec9
Roumen Damianoff <dev@roumen.it>
2016-09-09 21:16:27 +0300

    updated email

--- src/Roumen/Feed/Feed.php
+++ src/Roumen/Feed/Feed.php
@@ -3,7 +3,7 @@
 /**
  * Feed generator class for laravel-feed package.
  *
- * @author Roumen Damianoff <roumen@dawebs.com>
+ * @author Roumen Damianoff <roumen@crimsson.com>
  * @version 2.10.5
  * @link https://roumen.it/projects/laravel-feed
  * @license http://opensource.org/licenses/mit-license.php MIT License
