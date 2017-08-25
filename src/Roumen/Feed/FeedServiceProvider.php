6d5175cd78b07515631492b58b279c5b3687f6c2
Rumen Damyanov <r@alfamatter.com>
2017-08-25 21:28:13 +0300

    updated files indentation
    updated files indentation

--- src/Roumen/Feed/FeedServiceProvider.php
+++ src/Roumen/Feed/FeedServiceProvider.php
@@ -5,50 +5,50 @@ use Illuminate\Support\ServiceProvider;
 class FeedServiceProvider extends ServiceProvider
 {
 
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->loadViewsFrom(__DIR__ . '/../../views', 'feed');
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->loadViewsFrom(__DIR__ . '/../../views', 'feed');
 
-        $this->publishes([
-            __DIR__ . '/../../views' => base_path('resources/views/vendor/feed')
-        ], 'views');
-    }
+		$this->publishes([
+			__DIR__ . '/../../views' => base_path('resources/views/vendor/feed')
+		], 'views');
+	}
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->bind('feed', function()
-        {
-            return new Feed();
-        });
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind('feed', function()
+		{
+			return new Feed();
+		});
 
-        $this->app->alias('feed', 'Roumen\Feed\Feed');
-    }
+		$this->app->alias('feed', 'Roumen\Feed\Feed');
+	}
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['feed'];
-    }
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return ['feed'];
+	}
 
 }
